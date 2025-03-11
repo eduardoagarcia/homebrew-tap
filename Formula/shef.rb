241313258f9009714369d6d88ec3ea13abe3e644
Eduardo A. Garcia <24eg14@gmail.com>
2025-03-11 15:44:10 -0500

    chore(main): fix git username

--- Formula/shef.rb
+++ Formula/shef.rb
@@ -7,11 +7,11 @@ class Shef < Formula
 
   # Intel Mac
   if Hardware::CPU.intel?
-    url "https://github.com/yourusername/shef/releases/download/v0.1.3/shef_darwin_amd64.tar.gz"
+    url "https://github.com/eduardoagarcia/shef/releases/download/v0.1.3/shef_darwin_amd64.tar.gz"
     sha256 "1dfb46b7b4fd830693b28ae92a89fdcd95865df75663ea35bca057194da3d0c4"
   # Apple Silicon
   else
-    url "https://github.com/yourusername/shef/releases/download/v0.1.3/shef_darwin_arm64.tar.gz"
+    url "https://github.com/eduardoagarcia/shef/releases/download/v0.1.3/shef_darwin_arm64.tar.gz"
     sha256 "7654e25b7b86cbfebaf57ffb34cda6604c6142b0356abdc51f671c4df8f1d959"
   end
 
