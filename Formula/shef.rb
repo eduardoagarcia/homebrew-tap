c65b03fd673f4296e2d6b12f44086f8d4d2c139d
Eduardo A. Garcia <24eg14@gmail.com>
2025-03-11 15:56:02 -0500

    fix(main): fix indentation

--- Formula/shef.rb
+++ Formula/shef.rb
@@ -1,29 +1,29 @@
 class Shef < Formula
-  desc "Shef is a powerful CLI tool for cooking up shell recipes."
-  homepage "https://github.com/eduardoagarcia/shef"
-  license "MIT"
+    desc "Shef is a powerful CLI tool for cooking up shell recipes."
+    homepage "https://github.com/eduardoagarcia/shef"
+    license "MIT"
 
-  version "v0.1.3"
+    version "v0.1.3"
 
-  # Intel Mac
-  if Hardware::CPU.intel?
-    url "https://github.com/eduardoagarcia/shef/releases/download/v0.1.3/shef_darwin_amd64.tar.gz"
-    sha256 "1dfb46b7b4fd830693b28ae92a89fdcd95865df75663ea35bca057194da3d0c4"
-  # Apple Silicon
-  else
-    url "https://github.com/eduardoagarcia/shef/releases/download/v0.1.3/shef_darwin_arm64.tar.gz"
-    sha256 "7654e25b7b86cbfebaf57ffb34cda6604c6142b0356abdc51f671c4df8f1d959"
-  end
-
-  def install
-      if Hardware::CPU.intel?
-        bin.install Dir["shef_darwin_amd64/*"].first => "shef"
-      else
-        bin.install Dir["shef_darwin_arm64/*"].first => "shef"
-      end
+    # Intel Mac
+    if Hardware::CPU.intel?
+        url "https://github.com/eduardoagarcia/shef/releases/download/v0.1.3/shef_darwin_amd64.tar.gz"
+        sha256 "1dfb46b7b4fd830693b28ae92a89fdcd95865df75663ea35bca057194da3d0c4"
+    # Apple Silicon
+    else
+        url "https://github.com/eduardoagarcia/shef/releases/download/v0.1.3/shef_darwin_arm64.tar.gz"
+        sha256 "7654e25b7b86cbfebaf57ffb34cda6604c6142b0356abdc51f671c4df8f1d959"
     end
 
-  test do
-    system "#{bin}/shef", "--version"
-  end
+    def install
+        if Hardware::CPU.intel?
+            bin.install Dir["shef_darwin_amd64/*"].first => "shef"
+        else
+            bin.install Dir["shef_darwin_arm64/*"].first => "shef"
+        end
+    end
+
+    test do
+        system "#{bin}/shef", "--version"
+    end
 end
