b5f14a1332e91c3e143d637e0c0b14074d1dd121
Eduardo A. Garcia <24eg14@gmail.com>
2025-03-13 19:29:49 -0500

    release(main): shef v0.1.5

--- Formula/shef.rb
+++ Formula/shef.rb
@@ -3,16 +3,16 @@ class Shef < Formula
     homepage "https://github.com/eduardoagarcia/shef"
     license "MIT"
 
-    version "v0.1.4"
+    version "v0.1.5"
 
     # Intel Mac
     if Hardware::CPU.intel?
-        url "https://github.com/eduardoagarcia/shef/releases/download/v0.1.4/shef_darwin_amd64.tar.gz"
-        sha256 "b40cae8e46040e88f6e372f3fb01e1d416f91ebfdbec99bac3760a3abe34fa05"
+        url "https://github.com/eduardoagarcia/shef/releases/download/v0.1.5/shef_darwin_amd64.tar.gz"
+        sha256 "49bb314cea5ef803e7f5d08fc8c65f55997c9ef88b6cdfd073a8300e0d287796"
     # Apple Silicon
     else
-        url "https://github.com/eduardoagarcia/shef/releases/download/v0.1.4/shef_darwin_arm64.tar.gz"
-        sha256 "b10e1497217729bb7858140eb60ee8ea29fb19341e14c180e7f375e776dab114"
+        url "https://github.com/eduardoagarcia/shef/releases/download/v0.1.5/shef_darwin_arm64.tar.gz"
+        sha256 "3d4c5a5e7ade9264e3caf21854f4f48a4cfa487e62654758e63019c932b8cee1"
     end
 
     def install
